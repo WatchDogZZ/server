fd8ca8d54a4026655c90aab8f636f4e713bcf163
Benjamin Barbesange <Benjamin Barbesange>
2016-11-06 18:38:00 +0100

    Modification de l'organisation

--- run_server.js
+++ run_server.js
@@ -1,23 +1,36 @@
-// Imports
-const http = require('http'),
-    express = require('express');
+/**
+ * @file run_server.js
+ * @author Benjamin Barbesange && Benoît Garçon
+ * 
+ * @brief This file creates the web apps and handle the routing.
+ */
 
+/// Packages imports
+const http = require('http');
+const express = require('express');
+
+/// Service files imports
 const service = require('./service_methods.js');
 
 
-// Constants
+/// Configuration constants
 const SERVER_PORT = 3000;
 
 
-// Create the app
+/// Create the app
 var app = express();
 
 
-// Set properties
+// Set the app properties
+app.locals.title = 'WatchDogZZ Web service';
 app.set('port', SERVER_PORT);
 
+/******************************************************************************/
+// Routing
+/******************************************************************************/
 
 // Handle get request on root
+// Return simple html with greetings
 app.get('/', (request, response) => {
     response.send(`
     <html>
@@ -28,13 +41,45 @@ app.get('/', (request, response) => {
     `);
 });
 
+// '/where/user'
+// Return the location of a specific user. If user is ommited, return the positions
+// of all connected users. If the user does not exist
+app.get('/where/:user?', (request, response) => {
 
-app.get('/list', (request, response) => {
-    response.send(service.getList());
+    if(undefined !== request.params.user) {
+
+        var currentUser = service.getList().find( (el, idx, arr) => {
+            return el.name == request.params.user;
+        });
+
+        if(undefined !== currentUser) {
+            response.send(currentUser.position);
+        } else {
+            response.send( service.defaultLocation );
+        }
+
+    } else {
+
+        response.send( service.getList() );
+    }
+
+    response.end();
 });
 
-// Create the server
+// '/who'
+// Return the list of all connected users
+app.get('/who', (request, response) => {
+    response.send(service.getUsers());
+});
+
+
+/******************************************************************************/
+// Create the http server with the app
+/******************************************************************************/
+
+// The main http app
 http.createServer(app).listen(app.get('port'), () => {
-    // Callback when the server is ready
-    console.log('Server listening on port ' + app.get('port'));
-})
+    // When the app is running
+    console.log('[HTTP] app listening on port ' + app.get('port'));
+
+});
--- service_methods.js
+++ service_methods.js
@@ -1,8 +1,28 @@
+// This file contains the service methods that interact with the data
+
+var data = require('./data_stub.js');
 
 // Methode exemple
 exports.getList = function () {
-    return {
-        "ben": "0.0,0.0",
-        "benji": "10.0,10.0"
-    };
+    return data.list;
 }
+
+exports.getUsers = function() {
+    return data.users;
+}
+
+exports.addUser = function(name, position=[0.0, 0.0]) {
+    if(!data.users.include(name)) {
+        data.users.push(name);
+        
+        var newElt = {};
+        newElt["name"] = name;
+        newElt["position"] = position;
+
+        data.list.push(newElt);
+    }
+
+}
+
+/// Default location for a user
+exports.defaultLocation = [0.0, 0.0];
