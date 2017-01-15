9a8cd8b25dea9ee26326684f18975b915b79047b
Benjamin Barbesange <Benjamin Barbesange>
2017-01-15 16:11:16 +0100

    Added routing for login/logout/update

--- server.js
+++ server.js
@@ -9,6 +9,7 @@
 /// Packages imports
 var http = require('http');
 var express = require('express');
+var bodyParser = require('body-parser');
 
 /// Service files imports
 var service = require('./service.methods.js');
@@ -21,33 +22,124 @@ var SERVER_PORT = process.env.PORT || 80;
 /// Create the app
 var app = express();
 
+/******************************************************************************/
+// Config
+/******************************************************************************/
 
-// Set the app properties
 app.locals.title = 'WatchDogZZ Web service';
 app.set('port', SERVER_PORT);
+
+app.use(bodyParser.json()); // Parse JSON and place it in body
+
+// Middleware for each request
+// Adding header properties
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//   next();
+// });
+
 
 /******************************************************************************/
 // Routing
 /******************************************************************************/
 
+// GET '/'
 // Handle get request on root
 // Return simple html with greetings
 app.get('/', function (request, response) {
-    response.send('<html><body><h1>Welcome to WatchDogZZ web service home!</h1></body></html>');
+    response.send(`
+    <html>
+        <body>
+            <h1>Welcome to WatchDogZZ web service home!</h1>
+        </body>
+    </html>
+    `);
 
     response.end();
 });
 
-// Middleware for each request
-// Adding header properties
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next();
+// POST '/login'
+// Will parse the JSON body and create the user in the db
+// If the user uses other methods without the login, it will receive an error
+app.post('/login', function (request, response) {
+
+    var name = request.body.name || null;
+    var location = request.body.location || null;
+
+    if (null != name && null != location) {
+        var user = new serviceEntities.User(name, location);
+
+        service.login(user, function (err, res) {
+            
+            if (null == err) {
+                response.status(200);
+                response.send({
+                    'status': 'ok'
+                });
+
+            } else {
+                response.status(503);
+                response.send({
+                    'status': 'fail',
+                    'error': 'Database error'
+                });
+
+            }
+
+            response.end();
+        });
+
+    } else {
+        response.status(400)
+        response.send({
+            'status': 'fail'
+        });
+        response.end();
+    }
+
 });
 
-// '/where/user'
+
+// POST '/logout'
+// Parse the JSON body and delete the user from the database
+// The user will disapear from the map on the phone
+app.post('/logout', function (request, response) {
+
+    var name = request.body.name || null;
+
+    if (null != name) {
+        service.logout(request.body.name, function (err, res) {
+            
+
+            if (null == err) {
+                response.status(200);
+                response.send({
+                    'status': 'ok'
+                });
+            } else {
+                response.status(503);
+                response.send({
+                    'status': 'fail',
+                    'error': 'Database error'
+                });
+            }
+
+            response.end();
+        });
+    } else {
+        response.status(400);
+        response.send({
+            'status': 'fail'
+        });
+        response.end();
+    }
+    
+
+});
+
+// GET '/where/user'
 // Return the location of a specific user. If user is ommited, return the positions
 // of all connected users. If the user does not exist
 app.get('/where/:user?', function (request, response) {
@@ -55,7 +147,7 @@ app.get('/where/:user?', function (request, response) {
     service.getUserLocationList(function (err, serviceList) {
 
         // TODO : error logger
-        
+
         if (undefined !== request.params.user) {
             // If a user is specified, filter it
 
@@ -65,12 +157,14 @@ app.get('/where/:user?', function (request, response) {
 
             if (undefined !== currentUser) {
                 // User found, get the information and format it
+                response.status(200);
                 response.send({
                     'name': currentUser.name,
                     'location': currentUser.location
                 });
             } else {
                 // No user matching, sending default
+                response.status(200);
                 response.send({
                     'name': serviceEntities.defaultName,
                     'location': serviceEntities.defaultLocation
@@ -79,7 +173,7 @@ app.get('/where/:user?', function (request, response) {
 
         } else {
             // If there is no filter, send back the full list
-
+            response.status(200);
             response.send({
                 'list': serviceList
             });
@@ -89,13 +183,53 @@ app.get('/where/:user?', function (request, response) {
     });
 });
 
-// '/users'
+// POST '/where'
+// Update the user position
+// Pass a JSON containing the name, and the location
+app.post('/where', function (request, response) {
+    
+    var name = request.body.name || null;
+    var location = request.body.location || null;
+
+    if (null != name && null != location) {
+
+        service.updateUserLocation(name, location, function (err, res) {
+            
+            if (null == err) {
+                response.status(200);
+                response.send({
+                    'status': 'ok'
+                });
+                response.end();
+
+            } else {
+                response.status(503);
+                response.send({
+                    'status': 'fail',
+                    'error': 'Database error'
+                });
+                response.end();
+
+            }
+        });
+        
+    } else {
+        response.status(400);
+        response.send({
+            'status': 'fail'
+        });
+        response.end();
+    }
+
+});
+
+// GET '/users'
 // Return the list of all connected users
 app.get('/users', function (request, response) {
 
     service.getUsernameList(function (err, res) {
-        
-        
+
+
         response.send({
             'list': res
         });
@@ -104,8 +238,9 @@ app.get('/users', function (request, response) {
         response.end();
 
     });
-    
+
 });
+
 
 
 /******************************************************************************/
